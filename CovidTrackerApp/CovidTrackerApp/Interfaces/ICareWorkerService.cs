1a9422b731f0f7dfd9ed5fbf64a1a70aeaf03590
Jacob Stoneman (stonemaj) <jacob679@hotmail.co.uk>
2020-12-04 13:55:15 +0000

    Added careworker service layer

---
+++ CovidTrackerApp/CovidTrackerApp/Interfaces/ICareWorkerService.cs
@@ -0,0 +1,22 @@
+﻿using CovidTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace CovidTrackerApp.Interfaces
+{
+	public interface ICareWorkerService
+	{
+		bool CareWorkerExists(int id);
+		Task SaveChangesAsync();
+		Task<CareWorker> FindAsync(int? id);
+		void RemoveCareWorker(CareWorker c);
+		Task<CareWorker> GetCareWorkerByIdAsync(int? id);
+		public IEnumerable<Venue> GetVenues();
+		public void UpdateCareWorker(CareWorker c);
+		public void AddCareWorker(CareWorker c);
+		public IIncludableQueryable<CareWorker,Venue> GetCareworkers();
+	}
+}
